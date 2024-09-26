467400f6783c2dd2bfc4533104d98e9df8439e53
Pol-Mattis HARQUET <pol-mattis.harquet@epitech.eu>
2024-09-26 22:34:00 +0200

    test1

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Pol-Mattis HARQUET</title>
+	<link rel="icon" href="images/triforce-logo.png">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+</head>
+<body>
+
+	<div class="fenetre">
+		<div class="image-fond masque-parchemin" style="background-image: url('images/fond2-zelda.jpg')">
+			<div id="image-perso"></div>
+		</div>
+		<div class="infos">
+			<div class="h-nom-prenom">
+				<h1 class="zelda-font zelda-h1 h-nom">Harquet</h1>
+				<h1 class="zelda-font zelda-h2 h-prenom">Pol-Mattis</h1>
+			</div>
+			<div class="liste-nav">
+				<menu class="menu-navigation">
+					<a href="#"><li class="active">
+						<div class="hylia-font zelda-a1">A propos</div>
+						<div class="txt-sheikah sheikah-font zelda-a2">a-propos</div>
+					</li></a>
+					<a href="#"><li>
+						<div class="hylia-font zelda-a1">Profil</div>
+						<div class="txt-sheikah sheikah-font zelda-a2">profil</div>
+					</li></a>
+					<a href="#"><li>
+						<div class="hylia-font zelda-a1">Competences</div>
+						<div class="txt-sheikah sheikah-font zelda-a2">competences</div>
+					</li></a>
+					<a href="#"><li>
+						<div class="hylia-font zelda-a1">Projets</div>
+						<div class="txt-sheikah sheikah-font zelda-a2">projets</div>
+					</li></a>
+					<a href="#"><li>
+						<div class="hylia-font zelda-a1">Contact</div>
+						<div class="txt-sheikah sheikah-font zelda-a2">contact</div>
+					</li></a>
+				</menu>
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript">
+		$('.menu-navigation a li').on('mouseenter', function() {
+			var $this = $(this);
+
+			// Proceed only if the item is not already active
+			if (!$this.hasClass('active')) {
+				// Remove 'active' from all, add to the current item
+				$('.menu-navigation a li').removeClass('active');
+				$this.addClass('active');
+
+				// Get the index and set the image as before
+				var index = $('.menu-navigation a li').index(this);
+				var images = [
+					'images/heros-link1.png',
+					'images/heros-mipha1.png',
+					'images/heros-ravali1.png',
+					'images/heros-urbosa1.png',
+					'images/heros-daruk1.png',
+					'images/heros-zelda1.png'
+				];
+				var image = images[index] || 'images/heros-zelda1.png';
+
+				// Change the background image and restart the animation
+				$('#image-perso').css('background-image', 'url(' + image + ')');
+				$('#image-perso').removeClass('animate-arriver-droite');
+				void $('#image-perso')[0].offsetWidth;
+				$('#image-perso').addClass('animate-arriver-droite');
+
+	            // Jouer le son de clic (créer une nouvelle instance à chaque fois)
+	            var clickSound = new Audio('bruitages/click-button-140881.mp3');
+	            clickSound.play();
+			}
+		});
+	</script>
+
+</body>
+</html>
